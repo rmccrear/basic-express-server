99adc127bd1aace5a6b2534377eeddc95f1bf8c5
Robert Mccreary <fooyinfighter@gmail.com>
2022-09-20 13:35:55 -0700

    add person route

--- src/server.js
+++ src/server.js
@@ -2,9 +2,16 @@ const express = require("express");
 
 const app = express();
 
+app.get("/person", function(req, res){
+    const name = req.query.name;
+    console.log("req param name:", name);
+    res.setHeader('content-type', 'application/json');
+    res.send({name});
+})
+
 const start = function(opts) {
     const PORT = opts.PORT;
-    app.listen(PORT, () => console.log("Server Started"))
+    app.listen(PORT, () => console.log("Server Started"));
 }
 
-module.exports = {start}
+module.exports = {start};
